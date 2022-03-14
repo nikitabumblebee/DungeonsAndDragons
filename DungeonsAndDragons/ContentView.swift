16566f5c088ffe1f1d8e9edabc4b8ac3e8d8c443
NikitaBumblebee <nikitashmelev7@gmail.com>
2022-03-14 21:44:36 +0700

    Initial Commit

---
+++ DungeonsAndDragons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DungeonsAndDragons
+//
+//  Created by Nikita Shmelev on 3/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DungeonsAndDragons/DungeonsAndDragonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonsAndDragonsApp.swift
+//  DungeonsAndDragons
+//
+//  Created by Nikita Shmelev on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct DungeonsAndDragonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
